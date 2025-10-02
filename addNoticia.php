c1456dfa58645242b113a7e63d30f1abd6b73c9e
tralli18 <24dw.illart.beain@arangoya.net>
2025-10-02 10:16:32 +0200

    Se ha hecho el navbar desde cero, pero aun faltan cambios.

---
+++ addNoticia.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <title>Noticias Arangoya</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">Noticias Arangoya</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item me-3">
+            <a class="nav-link" href="index.php">Noticias</a>
+          </li>
+          <li class="nav-item me-3">
+            <a class="nav-link" href="addNoticia.php">Añadir Noticia</a>
+          </li>
+          <li class="nav-item me-3">
+            <a class="nav-link" href="contacto.php">Ayuda / Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <h2 class="text-center mt-4">Bienvenido a Noticias Arangoya</h2>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoA6VZ1GDZCBoYz9F1ZC1og6z4l5U5xU5z5x5z5x5z5x5z5" crossorigin="anonymous"></script>
+</body>
+
+</html>
