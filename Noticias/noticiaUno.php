e20c29f834c15e27c911aeacf81816f92d04d2a9
rabasot24 <24dw.alejandro.rabasot@arangoya.net>
2025-10-06 14:21:09 +0200

    Aplicado fixed-top para que el footer este fijado abajo, noticias uno empezada, cambiado el color del footer y puesto el favicon

--- Noticias/noticiaUno.php
+++ Noticias/noticiaUno.php
@@ -1,9 +1,10 @@
 <?php
-  $paginaActiva = "entretenimiento"; 
+$paginaActiva = "noticias";
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -11,40 +12,61 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   <link rel="stylesheet" href="/Github/Portal-de-Noticias-del-Centro/style.css" />
+  <link rel="stylesheet" href="../style.css">
+  <link rel="icon" href="../img/logo.png">
 
 </head>
-<body>
- <nav class="navbar navbar-expand-lg navbar-custom">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Noticias DAW</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
-        <li class="nav-item">
-          <a class="nav-link <?php echo ($paginaActiva == 'todas') ? 'active' : ''; ?>" href="http://localhost/Github/Portal-de-Noticias-del-Centro/index.php">Todas</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link <?php echo ($paginaActiva == 'politica') ? 'active' : ''; ?>" href="http://localhost/Github/Portal-de-Noticias-del-Centro/Noticias/Noticia.php">Política</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link <?php echo ($paginaActiva == 'tecnologia') ? 'active' : ''; ?>" href="http://localhost/Github/Portal-de-Noticias-del-Centro/Noticias/noticiaDos.php">Tecnología</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link <?php echo ($paginaActiva == 'deportes') ? 'active' : ''; ?>" href="http://localhost/Github/Portal-de-Noticias-del-Centro/Noticias/noticiaTres.php">Deportes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link <?php echo ($paginaActiva == 'entretenimiento') ? 'active' : ''; ?>" href="http://localhost/Github/Portal-de-Noticias-del-Centro/Noticias/addNoticia.php">Entretenimiento</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar" />
-        <button class="btn btn-outline-light" type="submit">Buscar</button>
-      </form>
-    </div>
-  </div>
-</nav>
 
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">
+        <img src="../img/logo.svg" alt="Logo"> Arangoya News
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link <?php echo ($paginaActiva == 'inicio') ? 'active' : ''; ?>"
+              href="../index.php">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php echo ($paginaActiva == 'noticias') ? 'active' : ''; ?>"
+              href="Noticias/noticiaUno.php">Noticias</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php echo ($paginaActiva == 'crear') ? 'active' : ''; ?>"
+              href="Noticias/noticiaDos.php">Añadir Noticia</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php echo ($paginaActiva == 'tecnologia') ? 'active' : ''; ?>"
+              href="Noticias/noticiaTres.php">Tecnología</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+<p>Prueba papa</p>
+<p>Prueba papa</p>
+
+<p>Prueba papa</p>
+
+<p>Prueba papa</p>
+
+<p>Prueba papa</p>
+
+
+
+  <footer class="bg-dark text-white text-center py-3 footer-custom fixed-bottom">
+    &copy; <?php echo date("Y"); ?> Portal de noticias Arangoya
+  </footer>
+
+  <!-- Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html> 
+
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-=======
 <?php
 // Definimos la página activa (puedes cambiar este valor dinámicamente)
 $paginaActiva = "inicio";
@@ -12,7 +9,8 @@ $paginaActiva = "inicio";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mini Portal de Noticias</title>
+    <title>Arangoya News</title>
+    <link rel="icon" href="img/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 
@@ -24,7 +22,7 @@ $paginaActiva = "inicio";
     <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
         <div class="container">
             <a class="navbar-brand" href="index.php">
-                <img src="img/logo.svg" alt="Logo"> Noticias
+                <img src="img/logo.svg" alt="Logo"> Arangoya News
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
@@ -149,8 +147,8 @@ $paginaActiva = "inicio";
 
 
     <!-- Footer -->
-    <footer class="bg-dark text-white text-center py-3">
-        &copy; <?php echo date("Y"); ?> Mini Portal de Noticias
+    <footer class="bg-dark text-white text-center py-3 footer-custom fixed-bottom">
+        &copy; <?php echo date("Y"); ?> Portal de noticias Arangoya
     </footer>
 
     <!-- Bootstrap JS -->
@@ -158,4 +156,3 @@ $paginaActiva = "inicio";
 </body>
 
 </html>
->>>>>>> 25c8e65bf1b037ec0957e158959d0b73de8276bc
